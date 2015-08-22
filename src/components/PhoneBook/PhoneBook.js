77bfa6203db22cfb95710611ea6a07239a430162
Vargentum <vargentum@gmail.com>
2015-08-22 13:46:25 +0300

    deploy phonebook component

---
+++ src/components/PhoneBook/PhoneBook.js
@@ -0,0 +1,16 @@
+import _ from "lodash";
+import React from "react";
+import cls from "classnames";
+
+
+export default class PhoneBook extends React.Component {
+  constructor(args) {
+    super(args)
+  }
+
+  render() {
+    return (
+      <h1>Hello</h1>
+    )
+  }
+}
